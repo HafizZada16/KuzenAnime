0b6061b5d94cd2531092f50f5611cc2c0cae83d3
HafizZada16 <hafizzada16@gmail.com>
2026-02-15 23:03:13 +0700

    yg bener dari web

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // Konfigurasi Alamat API saat Publish
 export const ANIME_API = "https://api.kanata.web.id/otakudesu"; // API Anime Luar
-export const USER_API = "https://api.hapizweb.my.id/api"; // API Backend Kamu
+export const USER_API = "https://api.kuzen.my.id/api"; // API Backend Kamu
