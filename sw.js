925c8e5c2e69072f78b264017476435cd7c38965
HafizZada16 <hafizzada15@gmail.com>
2026-03-13 20:03:21 +0700

    up13

--- sw.js
+++ sw.js
@@ -1,26 +1,26 @@
-const CACHE_NAME = "kuzen-cache-v2";
+const CACHE_NAME = "kuzen-cache-v3";
 
 // Saat di-install, Service Worker langsung aktif tanpa menunggu
 self.addEventListener("install", (event) => {
-  self.skipWaiting();
+    self.skipWaiting();
 });
 
 // Bersihkan cache lama jika ada versi baru
 self.addEventListener("activate", (event) => {
-  event.waitUntil(clients.claim());
+    event.waitUntil(clients.claim());
 });
 
 // Strategi: Jaringan selalu yang utama, kalau internet mati, baru cari di cache
 self.addEventListener("fetch", (event) => {
-  // Kita lewati request yang berasal dari API eksternal atau video agar tidak membebani storage HP
-  if (
-    event.request.url.includes("api.kanata") ||
-    event.request.url.includes("mp4")
-  ) {
-    return;
-  }
+    // Kita lewati request yang berasal dari API eksternal atau video agar tidak membebani storage HP
+    if (
+        event.request.url.includes("api.kanata") ||
+        event.request.url.includes("mp4")
+    ) {
+        return;
+    }
 
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request)),
-  );
+    event.respondWith(
+        fetch(event.request).catch(() => caches.match(event.request)),
+    );
 });
