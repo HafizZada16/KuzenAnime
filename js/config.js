d28353a78141bca7167ee3e07938c0c418ee744e
HafizZada16 <hafizzada15@gmail.com>
2026-03-17 10:34:00 +0700

    api domain backup up

--- js/config.js
+++ js/config.js
@@ -2,4 +2,4 @@
 export const ANIME_API = "https://api.kanata.web.id/otakudesu"; // API Anime Luar
 export const SANKA_API = "https://www.sankavollerei.com/anime";
 export const USER_API = "https://api.kuzen.my.id/api"; // API Backend Kamu
-export const USER_API_BACKUP = "https://api-kuzenanime.vercel.app/api"; // API Backend Backup
+export const USER_API_BACKUP = "https://api.kuzen.my.id/api"; // API Backend Backup
